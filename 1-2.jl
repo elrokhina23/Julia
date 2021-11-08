34ad9fa76535a5a263f5bacb0100bd730af36628
elrokhina23 <93782998+elrokhina23@users.noreply.github.com>
2021-11-08 11:05:30 +0300

    Update 1-2.jl
    Были добавлены номер задачи, дано, результат. Были переименованы основные функции.

--- 1-2.jl
+++ 1-2.jl
@@ -1,25 +1,29 @@
-function aaa(r::Robot,side::HorizonSide)
+# Задача № 1
+# ДАНО: Робот находится в произвольной клетке ограниченного прямоугольного поля без внутренних перегородок и маркеров.
+# РЕЗУЛЬТАТ: Робот — в исходном положении в центре прямого креста из маркеров, расставленных вплоть до внешней рамки.
+
+function marker_line(r::Robot,side::HorizonSide)
     while isborder(r, side) == false
         move!(r, side)
         putmarker!(r)
     end
 end
 
-function bbb(r::Robot,side::HorizonSide)
+function return_by_tokens(r::Robot,side::HorizonSide)
     while ismarker(r) == true
         move!(r, side)
     end        
 end
 
 function krest(r::Robot)
-    aaa(r, Nord)
-    bbb(r, Sud)
-    aaa(r, Sud)
-    bbb(r, Nord)
-    aaa(r, Ost)
-    bbb(r, West)
-    aaa(r, West)
-    bbb(r, Ost)
+    marker_line(r, Nord)
+    return_by_tokens(r, Sud)
+    marker_line(r, Sud)
+    return_by_tokens(r, Nord)
+    marker_line(r, Ost)
+    return_by_tokens(r, West)
+    marker_line(r, West)
+    return_by_tokens(r, Ost)
     putmarker!(r)
     
 end 
