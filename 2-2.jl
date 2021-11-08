d5fa876c4b12e2d3ede5c6580aa5880fcb3fe801
elrokhina23 <93782998+elrokhina23@users.noreply.github.com>
2021-11-08 11:52:21 +0300

    Update 2-2.jl
    Был добавлен счётчик шагов в функцию do_upora. Была добавлена функция move_steps, делающая заданное число шагов в заданном направлении. В функцию perimetr добавлены координаты правого нижнего угла(x,y). Далее маркеры ставятся по периметру и робот возвращается на изначальную позицию.

--- 2-2.jl
+++ 2-2.jl
@@ -7,8 +7,16 @@
 function do_upora(r::Robot, side::HorizonSide)
     while isborder(r, side)==false
         move!(r, side )
+        k+=1
     end
+    return k
 end
+
+function move_steps(r::Robot, side::HorizonSide, steps::Int)
+    for _ in 1:steps
+        move!(r,side)
+    end 
+end 
 
 
 function marker_line(r::Robot,side::HorizonSide)
@@ -19,13 +27,13 @@ function marker_line(r::Robot,side::HorizonSide)
 end
 
 function perimetr(r::Robot)
-    do_upora(r, Ost)
+    x = do_upora(r, Ost)
+    y = do_upora(r,Sud)
     marker_line(r,Nord)
     marker_line(r,West)
     marker_line(r,Sud)
     marker_line(r,Ost)
-    marker_line(r,Nord)
-    putmarker!(r)
-    
+    move_steps(r, Nord, y)
+    move_steps(r, West, x)
 end 
 
