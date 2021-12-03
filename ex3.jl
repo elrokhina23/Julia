05086223c6225b7366547e13fe2ec959f3d5320d
elrokhina23 <93782998+elrokhina23@users.noreply.github.com>
2021-12-03 10:10:14 +0300

    ex3
    Решение задачи №3 по основам программирования на языке программирования Julia

---
+++ ex3.jl
@@ -0,0 +1,47 @@
+function do_upora(r::Robot, side::HorizonSide)
+    k = 0
+    while isborder(r, side)==false
+        move!(r, side )
+        k+=1
+    end
+    return k
+end
+
+function move_steps(r::Robot, side::HorizonSide, steps::Int)
+    for _ in 1:steps
+        move!(r,side)
+    end 
+end 
+
+
+function marker_line(r::Robot,side::HorizonSide)
+    while isborder(r, side) == false
+        move!(r, side)
+        putmarker!(r)
+    end
+end
+
+function zmeika(r::Robot)
+    x = do_upora(r,West)
+    y = do_upora(r,Sud)
+    for i in 1:y
+        putmarker!(r)
+        marker_line(r,Ost)
+        if isborder(r,Nord) == false 
+            move!(r,Nord)
+        else 
+            break
+        end
+        putmarker!(r)
+        marker_line(r,West)
+        if isborder(r,Nord) == false
+            move!(r,Nord)
+        else 
+            break
+        end
+    end
+    do_upora(r,West)
+    do_upora(r,Sud)
+    move_steps(r,Ost,x)
+    move_steps(r,Nord,y)
+end
