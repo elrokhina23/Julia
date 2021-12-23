1bc0412e138f6487d03b57348255eb77b6fd2b43
elrokhina23 <93782998+elrokhina23@users.noreply.github.com>
2021-12-23 23:58:54 +0300

    Задача 10

---
+++ ex10.jl
@@ -0,0 +1,30 @@
+function main10!(::Robot)
+    a = []
+    summa = 0 
+    side = Ost 
+    while !isborder(r, Nord)
+    moves!(r, side, a)
+      if side == Ost
+        side = West
+      else
+        side = Ost
+      end
+    end
+    moves!(r, side, a)
+    k = length(a)
+    while length(a) > 0 
+      summa += pop!(a)
+    end
+  end
+  
+  function moves!(::Robot, side, a)
+    while !isborder(r, side)
+      if ismarker(r)
+        push!(a, temperature(r))
+      end
+      move!(r,side)
+    end
+    if !isborder(r, Nord)
+      move!(r, Nord)
+    end
+  end
